3c5e79026b734138a8291e754dd65a042452faf3
carlosmartde <carlosmartde@gmail.com>
2024-02-23 21:25:09 -0600

    prueba 2

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+int opcion;
+    do {
+        cout<<"================MENU================"<<endl;
+        cout<<"OPCION 1."<<endl;
+        cout<<"OPCION 2."<<endl;
+        cout<<"OPCION 3."<<endl;
+        cout<<"OPCION 4."<<endl;
+        cout<<"OPCION 5."<<endl;
+        cout<<"OPCION 6."<<endl;
+        cout<<"OPCION 7."<<endl;
+        cout<<"OPCION 8."<<endl;
+
+
+    }while (opcion !=8);
+
+
+
+
+
+
+    return 0;
+}
